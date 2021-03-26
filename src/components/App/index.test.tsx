1a89bbcdbec861c010d2790ab0b8b5b26b622e5a
samdjro <samdjro@users.noreply.github.com>
2021-03-26 18:17:15 +0100

    Add test coverage

--- src/components/App/index.test.tsx
+++ src/components/App/index.test.tsx
@@ -3,9 +3,17 @@ import {screen, render} from '@testing-library/react';
 import { App } from './index';
 
 describe('App', () => {
-  it('should render h1', () => {
+
+  it('should render h1 with title parameter', () => {
     const title = 'Testing!';
     render(<App title={title} />);
+    expect(screen.getByRole('heading')).toBeTruthy();
     expect(screen.getByRole('heading')).toHaveTextContent(title);
   });
+
+  it('should render h1 without title parameter', () => {
+    render(<App />);
+    expect(screen.getByRole('heading')).toBeTruthy();
+  });
+
 })
